48ca6487f2fc4fb918cd5f96b526c719478dc487
erick-pessoa <erick.pessoa.2014@hotmail.com>
2026-07-16 22:39:22 -0300

    Inicio de tudo

---
+++ Exercício_Programação/hello.js
@@ -0,0 +1,15 @@
+function inaraamor() {
+  var amor = document.getElementsByTagName("h1");
+
+  for (var i = 0; i < amor.length; i++) {
+    amor[i].style.backgroundColor = "yellow";
+  }
+}
+
+function Remove() {
+  var amor = document.getElementsByTagName("h1");
+
+  for (var i = 0; i < amor.length; i++) {
+    amor[i].style.backgroundColor = "";
+  }
+}
