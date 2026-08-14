85dd3874ad33c3e736716547ac56b6f8b14baae9
erick-pessoa <erick.pessoa.2014@hotmail.com>
2026-08-14 19:57:23 -0300

    Projeto terminado

--- Exercício_Projeto/EnvioDeEmail/script.js
+++ Exercício_Projeto/EnvioDeEmail/script.js
@@ -15,14 +15,41 @@ document.getElementById("form").addEventListener("submit", function (event) {
   const serviuceID = "service_a6ckn2e";
   const templateID = "template_5auanj9";
   const button_enviar = document.getElementById("button_enviar");
-
   button_enviar.textContent = "Enviando...";
+  button_enviar.disabled = true;
 
   emailjs.send(serviuceID, templateID, formData).then(() => {
-      alert("Enviado com Sucesso!");
-      button_enviar.textContent = "Enviar";
+    
+    Toastify({
+          text: "Enviado com sucesso!",
+          duration: 3000,
+          style:{
+            background: "#28a745",
+            color: "#fff"
+          }
+
+      }).showToast();
+
+      document.getElementById("form").reset();
+      
     }).catch((error) => {
-      alert("Occorreu um erro ao enviar o email: " + error);
+
+     Toastify({
+          text: "Erro ao enviar",
+          duration: 3000,
+          style:{
+            background: "red",
+            color: "#fff"
+          }
+
+      }).showToast();
+      
+
+    }).finally(() => {
       button_enviar.textContent = "Enviar";
+        button_enviar.disabled = false;
+
+      
+
     });
 });
