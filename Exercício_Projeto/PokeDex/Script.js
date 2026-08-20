acc43bec3d3ee56d6a2a2ef7b9cf43bd8901d9c1
erick-pessoa <erick.pessoa.2014@hotmail.com>
2026-08-20 15:27:55 -0300

    Interagindo com imagem

--- Exercício_Projeto/PokeDex/Script.js
+++ Exercício_Projeto/PokeDex/Script.js
@@ -1,3 +1,27 @@
-const fetchPokemon = (pokemon)=>{
-    const
-}
+const pokemonName = document.querySelector(".pokemon_name");
+const pokemonNumber = document.querySelector(".pokemon_number");
+const pokemonImagem = document.querySelector(".pokemon");
+const inputSearch = document.querySelector(".input_search");
+
+/* Buscando API*/
+const fetchPokemon = async (pokemon) => {
+  const APIResponse = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`,
+  );
+  /*Convertenbd para JSON*/
+  const data = await APIResponse.json();
+  return data;
+};
+
+const renderisaPokemon = async (pokemon) => {
+  const data = await fetchPokemon(pokemon);
+
+  pokemonName.innerHTML = data.name;
+  pokemonNumber.innerHTML = data.id;
+  pokemonImagem.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+};
+
+inputSearch.addEventListener("input", async (event)=>{
+    const pokemon = event.target.value;
+    renderisaPokemon(pokemon);
+})
