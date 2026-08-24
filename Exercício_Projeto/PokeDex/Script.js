64333775239fe0182f9af2e4db46523e51ee16ec
erick-pessoa <erick.pessoa.2014@hotmail.com>
2026-08-24 18:46:10 -0300

    Falta colocar os botões

--- Exercício_Projeto/PokeDex/Script.js
+++ Exercício_Projeto/PokeDex/Script.js
@@ -1,27 +1,47 @@
 const pokemonName = document.querySelector(".pokemon_name");
 const pokemonNumber = document.querySelector(".pokemon_number");
 const pokemonImagem = document.querySelector(".pokemon");
-const inputSearch = document.querySelector(".input_search");
+const input_search = document.querySelector(".input_search");
+const btn_next = document.querySelector(".btn-next");
+const btn_prev = document.querySelector(".btn-prev");
 
 /* Buscando API*/
 const fetchPokemon = async (pokemon) => {
   const APIResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemon}`,
   );
-  /*Convertenbd para JSON*/
-  const data = await APIResponse.json();
-  return data;
+  if (APIResponse.status === 200) {
+    /*Convertenbd para JSON*/
+    const data = await APIResponse.json();
+    return data;
+  }
 };
 
 const renderisaPokemon = async (pokemon) => {
+
+  pokemonName.innerHTML = "Loading...";
+  pokemonNumber.innerHTML = "";
+
   const data = await fetchPokemon(pokemon);
 
-  pokemonName.innerHTML = data.name;
-  pokemonNumber.innerHTML = data.id;
-  pokemonImagem.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+  if (data) {
+    pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImagem.src =
+      data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
+        "front_default"
+      ];
+  } else {
+    pokemonName.innerHTML = "not found :(";
+    pokemonNumber.innerHTML = "";
+    pokemonImagem.src = "";
+    
+  }
 };
 
-inputSearch.addEventListener("input", async (event)=>{
-    const pokemon = event.target.value;
-    renderisaPokemon(pokemon);
-})
+input_search.addEventListener('input', (event) => {
+  event.preventDefault();
+  renderisaPokemon(input_search.value);
+});
+
+renderisaPokemon(1);
