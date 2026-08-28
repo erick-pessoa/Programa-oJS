05ffdcd0f0c0a90cb568fb4144c8ebdf3b4c9618
erick-pessoa <erick.pessoa.2014@hotmail.com>
2026-08-28 20:03:40 -0300

    Colocando as frases na tela

---
+++ Exercício_Projeto/BarraDePesquisa/script.js
@@ -0,0 +1,40 @@
+const data = [
+  {
+    title: "Minha casa é grande",
+    description: "Minha casa é muito grande mesmo, bando de porra",
+  },
+  {
+    title: "NEWworld melhora casa branca",
+    description: "Minha casa é muito grande mesmo, bando de porra",
+  },
+  {
+    title: "NEWworld",
+    description: "Minha casa é muito grande mesmo, bando de porra",
+  },
+  {
+    title: "Helllo",
+    description: "Minha casa é muito grande mesmo, bando de porra",
+  },
+  {
+    title: "Inara te amo, coisa fofa",
+    description: "Minha casa é muito grande mesmo, bando de porra",
+  },
+];
+
+const cardContainer = document.querySelector(".card-container");
+const searchInput = document.querySelector("#searchInput");
+
+const displayData = (data) => {
+  cardContainer.innerHTML = "";
+
+  data.forEach((e) => {
+    cardContainer.innerHTML += `
+    <div class="card">
+    <h3>${e.title}</h3>
+    <p>${e.description}</p>
+    </div>
+    `;
+  });
+};
+
+window.addEventListener("load", displayData.bind(null, data));
