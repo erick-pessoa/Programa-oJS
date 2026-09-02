4937a513e1379bc3ac904687d897c64996a416e8
erick-pessoa <erick.pessoa.2014@hotmail.com>
2026-09-02 18:48:20 -0300

    Colocando input do formulário em tela

--- Projetos_Próprio/CadastroClienteEsteticista/script.js
+++ Projetos_Próprio/CadastroClienteEsteticista/script.js
@@ -1,6 +1,27 @@
 const Janela_cliente = document.querySelector(".Janela_cliente");
 const Btn_cadastrar = document.querySelector(".btn-cadastrar");
+const btn_save = document.querySelector("#btn-salvar");
+const inputName = document.querySelector("#nome");
+const inputCpf = document.querySelector("#cpf");
+const inputEmail = document.querySelector("#email");
+const inputTelefone = document.querySelector("#telefone");
 
 Btn_cadastrar.addEventListener("click", () => {
   Janela_cliente.classList.toggle("FechaJanela");
+});
+
+/* SAVE BUTTON*/
+btn_save.addEventListener("click", () => {
+
+  let createFormLi = document.createElement("li");
+  createFormLi.innerHTML = ` 
+ <li>Nome: ${inputName.value}</li>
+ <li>CPF: ${inputCpf.value}</li>
+ <li>EMAIL: ${inputEmail.value}</li>
+ <li>TELEFONE: ${inputTelefone.value}</li>
+ 
+  `
+  document.querySelector('#list_people').appendChild(createFormLi);
+  
+
 });
