9bf395f527f0c6783e020aa2a61b64edbeebd585
erick-pessoa <erick.pessoa.2014@hotmail.com>
2026-09-03 17:49:03 -0300

    Foi feito botão de excluir

--- Projetos_Próprio/CadastroClienteEsteticista/script.js
+++ Projetos_Próprio/CadastroClienteEsteticista/script.js
@@ -5,7 +5,11 @@ const inputName = document.querySelector("#nome");
 const inputCpf = document.querySelector("#cpf");
 const inputEmail = document.querySelector("#email");
 const inputTelefone = document.querySelector("#telefone");
+const inputText = document.querySelector("#textClient");
 
+let clientEdit = null;
+
+/* SHOWING WINDOW OF CLIENT*/
 Btn_cadastrar.addEventListener("click", () => {
   Janela_cliente.classList.toggle("FechaJanela");
 });
@@ -15,13 +19,34 @@ btn_save.addEventListener("click", () => {
 
   let createFormLi = document.createElement("li");
   createFormLi.innerHTML = ` 
- <li>Nome: ${inputName.value}</li>
- <li>CPF: ${inputCpf.value}</li>
- <li>EMAIL: ${inputEmail.value}</li>
- <li>TELEFONE: ${inputTelefone.value}</li>
- 
-  `
-  document.querySelector('#list_people').appendChild(createFormLi);
-  
+ <div>Nome: ${inputName.value}</div>
+ <div>CPF: ${inputCpf.value}</div>
+ <div>EMAIL: ${inputEmail.value}</div>
+ <div>TELEFONE: ${inputTelefone.value}</div>
+ <div>Comentário: ${inputText.value}</div>
 
+ <span id="remove" onclick="removeForm(this)">❌</span>
+ <button id="edite" onclick="EditForm(this)">✏️</button>
+ `;
+
+  document.querySelector("#list_people").appendChild(createFormLi);
+
+  document.querySelector("#nome").value = "";
+  document.querySelector("#cpf").value = "";
+  document.querySelector("#email").value = "";
+  document.querySelector("#telefone").value = "";
+  document.querySelector("#textClient").value = "";
 });
+
+/* REMOVE LIST OF PEOPLE*/
+const removeForm = (e) => {
+  e.parentElement.remove();
+};
+
+/* EDIT LIST OF PEOPLE*/
+const editForm = (e)=>{
+
+  clientEdit = e.parentElement;
+
+
+}
